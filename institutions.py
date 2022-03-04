749a215306eababe575a273c96396cbda5cc6d97
jackweyer <70491390+jackweyer@users.noreply.github.com>
2022-03-04 13:42:32 -0800

    Update institutions.py

--- institutions.py
+++ institutions.py
@@ -7,16 +7,23 @@ Original file is located at
     https://colab.research.google.com/drive/1WN9dIRsVsyoVnwA5xdGOWUYjLkeCTPtR
 """
 
+# Load in libraries
 import pandas as pd
 import json
 
+# Import U.S. Department of Education data
 df = pd.read_csv('MERGED2019_20_PP.csv')
 
+# Select desired columns
 universityData = df[["INSTNM", "LATITUDE", "LONGITUDE", "UGDS"]]
+# Rename columns
 universityData = universityData.rename(columns = {"INSTNM":'Institution', "LATITUDE":'Latitude', 
                                  "LONGITUDE":'Longitude', "UGDS":'UndergradStudentCount'})
 
+# Change our dataframe to JSON
 universityDataJSON = universityData.to_json(orient = 'records')
 parsed = json.loads(universityDataJSON)
+
+# Write to .json file
 with open('universityData.json', 'w') as json_file:
  json.dump(universityDataJSON, json_file)
